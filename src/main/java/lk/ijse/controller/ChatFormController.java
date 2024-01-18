c1d002336127aae1a7ca49e9c67c9e8dcfff099d
ShenanVindinu <shenanvindinu@gmail.com>
2024-01-18 16:50:06 +0530

    chat form updated

--- src/main/java/lk/ijse/controller/ChatFormController.java
+++ src/main/java/lk/ijse/controller/ChatFormController.java
@@ -3,13 +3,13 @@ package lk.ijse.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 
 public class ChatFormController {
 
     @FXML
-    private TextArea TextBox;
+    private ScrollPane ScrollPane;
 
     @FXML
     private Button imageButton;
