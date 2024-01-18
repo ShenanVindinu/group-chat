28ec6852c8495f4d9061d68d780ab86f0376ac6f
ShenanVindinu <shenanvindinu@gmail.com>
2024-01-18 16:22:10 +0530

    client chat form created

--- src/main/java/lk/ijse/Server.java
+++ src/main/java/lk/ijse/Server.java
@@ -4,7 +4,7 @@ public class Server {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
 
     }
 
---
+++ src/main/java/lk/ijse/controller/ChatFormController.java
@@ -0,0 +1,33 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ChatFormController {
+
+    @FXML
+    private TextArea TextBox;
+
+    @FXML
+    private Button imageButton;
+
+    @FXML
+    private TextField massageBox;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    void sendImage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMassage(ActionEvent event) {
+
+    }
+
+}
