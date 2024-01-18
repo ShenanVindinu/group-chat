4792a005b9e152dce045610d35d42bcd37eef055
ShenanVindinu <shenanvindinu@gmail.com>
2024-01-18 19:40:16 +0530

    chat form updated with scroll pane and Vbox

--- src/main/java/lk/ijse/controller/ChatFormController.java
+++ src/main/java/lk/ijse/controller/ChatFormController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class ChatFormController {
 
@@ -19,6 +21,12 @@ public class ChatFormController {
 
     @FXML
     private Button sendButton;
+
+    @FXML
+    private Text userNameText;
+
+    @FXML
+    private VBox vBox;
 
     @FXML
     void sendImage(ActionEvent event) {
